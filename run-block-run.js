ecfd1f41c8baa48eb0210d95753127802e8c1ef0
Andrew Davis <andrew@moodle.com>
2013-07-21 19:40:59 +0800

    numerous changes

--- run-block-run.js
+++ run-block-run.js
@@ -11,7 +11,11 @@ function drawRect(context, x, y, width, height, color) {
     context.fillStyle = color;
     context.fillRect(x, y, width, height);
 }
-
+function drawText(context, text, font, style, x, y) {
+    context.font = font;
+    context.fillStyle = style;
+    context.fillText(text, x, y);
+}
 function getRandomInt(minimum, maximum) {
     rand = minimum + Math.floor(Math.random() * (maximum - minimum + 1));
     return rand;
@@ -83,15 +87,18 @@ function getRandomInt(minimum, maximum) {
   };
 })();
 
+function getGameSeconds() {
+    return Math.floor((Date.now() - gGameTime)/1000);
+}
+
 Building = Class.extend({
     init: function(pos, size, color) {
         this.pos = pos;
         this.size = size;
         this.color = color;
     },
-    update: function(dt) {
-        multiplier = Math.floor((Date.now() - gGameTime)/1000) * 5;
-        this.pos[0] -= dt * multiplier;
+    update: function(dt, seconds) {
+        this.pos[0] -= dt * seconds * 5;//Math.ceil(dt * seconds * 5);
         
         //is this building still on screen?
         return (this.pos[0] + this.size[0]) > 0;
@@ -106,20 +113,35 @@ Building = Class.extend({
             }
         }
         return false;
+    },
+    rectangleCollide: function(r) {
+        var leftInside =  r.pos[0] > this.pos[0] && r.pos[0] < this.pos[0] + this.size[0];
+        var rightInside= r.pos[0] + r.size[0] > this.pos[0] && r.pos[0]  + r.size[0] < this.pos[0] + this.size[0];
+        
+        if (leftInside || rightInside) {
+             //only checking the bottom of r
+             var bottom = r.pos[1] + r.size[1];
+             if (bottom > this.pos[1] && bottom < this.pos[1] + this.size[1]) {
+                return true;
+             }
+        }
+        return false;
     }
 });
 
 City = Class.extend({
     buildings: null,
     spacer: 10,
+    previousHeight: null,
     init: function() {
         this.buildings = Array();
     },
     update: function(dt) {
         var toRemove = Array(), nextX = 0;
+        var seconds = getGameSeconds();
         
         for (var i = 0;i < this.buildings.length;i++) {
-            if (!this.buildings[i].update(dt)) {
+            if (!this.buildings[i].update(dt, seconds)) {
                 toRemove.push(i);
             } else {
                 if (nextX < this.buildings[i].pos[0] + this.buildings[i].size[0] + this.spacer) {
@@ -133,12 +155,36 @@ City = Class.extend({
         }
         //new buildings
         var pos = null, size = null;
+        var minHeight, nextHeight;
+        var minWidth, maxWidth;
         while (nextX < gCanvas.width) {
-            size = [getRandomInt(150, 400), getRandomInt(40, 250)];
+            if (this.buildings.length == 0) {
+                //make first building wide and low
+                nextHeight = getRandomInt(30, 50);
+                minWidth = 700;
+                maxWidth = 1000;
+            } else {
+                minHeight = this.previousHeight - 100;
+                if (minHeight < 30) {
+                    minHeight = 30;
+                }
+                nextHeight = null
+                while (!nextHeight) {
+                    nextHeight = getRandomInt(minHeight, this.previousHeight + 80);
+                    if (gCanvas.height - nextHeight < 20) {
+                        nextHeight = null;
+                    }
+                }
+                
+                minWidth = 50;
+                maxWidth = 1000;
+            }
+            size = [getRandomInt(minWidth, maxWidth), nextHeight];
             pos = [nextX, gCanvas.height - size[1]];
             this.buildings.push(new Building(pos, size, 'grey'));
             
             nextX = pos[0] + size[0] + this.spacer;
+            this.previousHeight = nextHeight;
         }
     },
     draw: function() {
@@ -149,10 +195,18 @@ City = Class.extend({
     pointCollide: function(p) {
         for (var i = 0;i < this.buildings.length;i++) {
             if (this.buildings[i].pointCollide(p)) {
-                return true;
+                return this.buildings[i];
             }
         }
-        return false;
+        return null;
+    },
+    rectangleCollide: function(r) {
+        for (var i = 0;i < this.buildings.length;i++) {
+            if (this.buildings[i].rectangleCollide(r)) {
+                return this.buildings[i];
+            }
+        }
+        return null;
     }
 });
 
@@ -160,14 +214,13 @@ Star = Class.extend({
     init: function(pos) {
         this.pos = pos;
     },
-    update: function(dt) {
-        multiplier = Math.floor((Date.now() - gGameTime)/1000);
-        this.pos[0] -= dt * multiplier;
+    update: function(dt, seconds) {
+        this.pos[0] -= dt * seconds;//Math.floor(dt * seconds);
         
         //is this star still on screen?
         return this.pos[0] > 0;
     },
-    draw: function(dt) {
+    draw: function() {
         drawRect(gContext, this.pos[0], this.pos[1], 1, 1, 'white');
     }
 });
@@ -180,9 +233,10 @@ StarField = Class.extend({
     },
     update: function(dt) {
         var toRemove = Array(), nextX = 0;
+        var seconds = getGameSeconds();
         
         for (var i = 0;i < this.stars.length;i++) {
-            if (!this.stars[i].update(dt)) {
+            if (!this.stars[i].update(dt, seconds)) {
                 toRemove.push(i);
             } else {
                 if (nextX < this.stars[i].pos[0] + this.spacer) {
@@ -212,39 +266,67 @@ StarField = Class.extend({
 });
 
 Block = Class.extend({
-    init: function(pos, size, color) {
+    init: function(pos, size, startSize, color, vel) {
         this.pos = pos;
-        this.size = size;
-        this.color = color;
-        
-        this.vel = [0, 0];
         this.originalsize = size;
+        this.size = startSize;
+        this.color = color;
+        this.vel = vel;
+        
+        this.airborne = true;
+        this.jumpsavailable = 2;
+        this.jumpsused = 0;
     },
     update: function(dt) {
         this.size[0] = this.snapback(this.originalsize[0], this.size[0]);
         this.size[1] = this.snapback(this.originalsize[1], this.size[1]);
         
-        var belowLeft  = [this.pos[0] - (this.size[0]/2), this.pos[1]+1];
-        var belowRight = [this.pos[0] + (this.size[0]/2), this.pos[1]+1];
-        var inAir = !gCity.pointCollide(belowLeft) && !gCity.pointCollide(belowRight);
-        
-        //gravity
-        if (inAir) {
-            this.vel[1] += 5;
+        //apply gravity if jumping or have slid off a building
+        if (!this.airborne) {
+            var belowLeft  = [this.pos[0] - (this.size[0]/2), this.pos[1] + 2];
+            this.airborne = !gCity.pointCollide(belowLeft);
+        }
+        if (this.airborne) {
+            this.vel[1] += 500 * dt;
+            this.airborne = true;
         }
         
         var oldpos = this.pos.slice(0);
-        this.pos[0] += this.vel[0]*dt;
-        this.pos[1] += this.vel[1]*dt;
+        //this.pos[0] += this.vel[0]*dt;
+        this.pos[1] += Math.round(this.vel[1] * dt);
         
-        var bottomLeft  = [this.pos[0] - (this.size[0]/2), this.pos[1]];
-        var bottomRight = [this.pos[0] + (this.size[0]/2), this.pos[1]];
-        if (gCity.pointCollide(bottomLeft) || gCity.pointCollide(bottomRight)) {
-            this.pos = oldpos;
-            
-            if (inAir && gCity.pointCollide(bottomLeft) && gCity.pointCollide(bottomRight)) {
-                this.vel[1] = 0;
-                this.squish();
+        if (this.pos[1] > gCanvas.height) {
+            newGame();
+        }
+        
+        var rect = {
+            'pos': [this.pos[0] - (this.size[0]/2), this.pos[1] - this.size[1]],
+            'size': this.size
+        };
+        var collide = gCity.rectangleCollide(rect);
+        if (collide) {
+            //if was over the building
+            //adding 1 as player was falling through sometimes
+            if (oldpos[1] < collide.pos[1]+1) {
+                var left = oldpos[0] - (this.size[0]/2);
+                var leftSideOver  = (left > collide.pos[0] && left < collide.pos[0] + collide.size[0]);
+                
+                var right = oldpos[0] + (this.size[0]/2);
+                var rightSideOver = (right > collide.pos[0] && right < collide.pos[0] + collide.size[0]);
+                
+                if (leftSideOver || rightSideOver) {
+                
+                    //landed on top of a building
+                    this.pos[1] = collide.pos[1];
+                    this.jumpsused = 0;
+                    if (this.vel[1] > 100) {
+                        this.squish();
+                    }
+                    this.vel[1] = 0;
+                    this.airborne = false;
+                }
+            } else {
+                //do nothing. Let player pass through building.
             }
         }
     },
@@ -257,13 +339,17 @@ Block = Class.extend({
         this.size = [this.size[0]*1.5, this.size[1]*0.5];
     },
     jump: function() {
-        //if not already in the air
-        
-        //one pixel under the bottom left corner
-        var underLeft = [this.pos[0] - this.size[0]/2, this.pos[1] + 1];
-        if (gCity.pointCollide(underLeft)) {
+        var jump = false;
+        if (!this.airborne) {
+            jump = true;
+        } else if (this.jumpsused < this.jumpsavailable && this.jumpsavailable > 1) {
+            jump = true;
+        }
+        if (jump) {
+            this.jumpsused++;
             this.size = [this.size[0]*0.5, this.size[1]*2];
-            this.vel[1] -= 400;
+            this.vel[1] -= 300;//400;
+            this.airborne = true;
         }
     },
     snapback: function(original, current) {
@@ -273,9 +359,9 @@ Block = Class.extend({
         }
 
         if (current < original) {
-            return current * 1.05;
+            return Math.round(current * 1.1);
         } else {
-            return current * 0.95;
+            return Math.round(current * 0.9);
         }
     }
 });
@@ -291,11 +377,32 @@ window.addEventListener('keydown', onKeyDown, false);
 
 var gCanvas = document.getElementById('gamecanvas');
 var gContext = gCanvas.getContext('2d');
+var gHighScore = 0;
+
+function newGame() {
+    if (gGameTime) {
+        if (getGameSeconds() > gHighScore) {
+            gHighScore = getGameSeconds();
+        }
+    }
+    gGameTime = Date.now();
+    
+    gCity = new City();
+    gStarField = new StarField();
+
+    var pos = [50, gCanvas.height-1];
+    var size = [30, 40];
+    gBlock = new Block(pos, size, [1000, 1000], 'red', [0, -270]);
+}
 
 function updateGame(dt) {
     gStarField.update(dt);
     gCity.update(dt);
     gBlock.update(dt);
+    
+    if (gBlock.jumpsavailable < 5) {
+        gBlock.jumpsavailable = Math.floor(getGameSeconds() / 60) + 1;
+    }
 }
 
 function drawGame() {
@@ -306,18 +413,42 @@ function drawGame() {
     gStarField.draw();
     gCity.draw();
     gBlock.draw();
+    
+    var seconds = getGameSeconds();
+    
+    if (seconds > 1 && seconds < 5) {
+        drawText(gContext, "Run Block Run", '36pt '+gFont, 'red', 130, gCanvas.height/2);
+    } else if (seconds > 6 && seconds < 10) {
+        drawText(gContext, "press J to jump", '24pt '+gFont, 'red', 180, gCanvas.height/2);
+    }
+    
+    text = seconds + " s";
+    drawText(gContext, text, '18pt '+gFont, 'green', gCanvas.width - 60, 40);
+    
+    if (gHighScore > 0) {
+        text = gHighScore + " s";
+        drawText(gContext, text, '18pt '+gFont, 'green', gCanvas.width - 60, 80);
+    }
+    
+    var color = null;
+    for (var i = 0; i < 5; i++) {
+        if (i < gBlock.jumpsavailable - gBlock.jumpsused) {
+            color = 'red';
+        } else {
+            color = 'grey';
+        }
+        drawRect(gContext, 50+(i*40), 20, 10, 10, color);
+    }
 }
+
+var gFont = "Courier New";
 
 var gOldTime = Date.now();
 var gNewTime = null;
-var gGameTime = Date.now();
+var gGameTime = null;
 
-var gCity = new City();
-var gStarField = new StarField();
-
-var pos = [30, 100];
-var size = [30, 40];
-var gBlock = new Block(pos, size, 'red');
+var gCity = null, gStarField = null, gBlock = null;
+newGame();
 
 //executed 60/second
 var mainloop = function() {
